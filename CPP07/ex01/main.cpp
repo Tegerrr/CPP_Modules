fdf2d6abdf9566b0b4aa8feead3122839b55acee
Tegerrr <ghost45566@gmail.com>
2024-10-06 18:48:37 +0400

    007

--- CPP07/ex01/main.cpp
+++ CPP07/ex01/main.cpp
@@ -1,17 +1,40 @@
 #include "Iter.hpp"
 #include <string>
 
-int main(void)
+// int main(void)
+// {
+// 	std::string str[3] = {
+// 		"lala",
+// 		"bebe",
+// 		"zuzu"
+// 	};
+// 	iter(str, 3, print);
+
+// 	int arr[3] = {1, 2, 3};
+// 	iter(arr, 3, print);
+
+// 	return (0);
+// }
+
+class Awesome
 {
-	std::string str[3] = {
-		"lala",
-		"bebe",
-		"zuzu"
-	};
-	iter(str, 3, print);
+	public:
+	Awesome( void ) : _n( 42 ) { return; }
+	int get( void ) const { return this->_n; }
+	private:
+	int _n;
+};
 
-	int arr[3] = {1, 2, 3};
-	iter(arr, 3, print);
+std::ostream & operator<<( std::ostream & o, Awesome const & rhs ) { o << rhs.get(); return o; }
+template< typename T >
+void print( T const & x ) { std::cout << x << std::endl; return; }
 
-	return (0);
+int main() 
+{
+int tab[] = { 0, 1, 2, 3, 4 }; // <--- I never understood why you can't write int[] tab. Wouldn't that make more sense?
+Awesome tab2[5];
+
+iter( tab, 5, print );
+iter( tab2, 5, print );
+return 0;
 }
